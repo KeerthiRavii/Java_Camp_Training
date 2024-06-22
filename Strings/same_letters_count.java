ae670d85a176f1116ebb051b93c53f5a6d5f12fe
Keerthi <keerthiravi304@gmail.com>
2024-06-21 22:11:24 -0400

    Committing the changes

---
+++ Strings/same_letters_count.java
@@ -0,0 +1,38 @@
+//Given an input string aaaabbBccdee. It can be written as a4b2B1c2d1e2.
+
+
+import java.util.Scanner;
+class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        //System.out.print(str.length());
+        
+        char[] ch=str.toCharArray();
+        int count=1;
+     //   int temp=ch[0];
+        for(int i=0;i<str.length();i++){
+           // temp=ch[0];
+            //System.out.print(ch[i]);
+            if(i>=str.length()-1){
+                System.out.print(ch[i]);
+                if(ch[i]==ch[str.length()-1])
+                count++;
+                System.out.print(count-1);
+                break;
+            }
+           
+          if(ch[i]==ch[i+1]){
+                count++;
+                //Syste
+            }
+          else{
+                System.out.print(ch[i]);
+                System.out.print(count);
+                count=1;
+            }
+            //System.out.print(ch[i]);
+        }
+    }
+}
+
